f023382a0c1e793441a5a112569243e82f220410
Zhan Ilyas <114327302+sweethava@users.noreply.github.com>
2026-06-19 15:14:47 +0500

    Add: design-linked-list - submission-9

---
+++ Data Structures & Algorithms/design-linked-list/submission-9.js
@@ -0,0 +1,154 @@
+class MyLinkedList {
+    length
+    head
+    tail
+
+    constructor() {
+        this.length = 0
+        this.head = this.tail = undefined
+    }
+
+    /**
+     * @param {number} index
+     * @return {ListNode}
+     */
+    getPrev(index) {
+        const item = this.getNode(index)
+
+        if (item) {
+            return item.prev
+        }
+    }
+
+    /**
+     * @param {number} index
+     * @return {number}
+     */
+    get(index) {
+        const item = this.getNode(index)
+
+        if (item) {
+            return item.value
+        }
+
+        return -1
+    }
+
+    /**
+     * @param {number} index
+     * @return {ListNode}
+     */
+    getNode(index) {
+        let i = 0
+        let current = this.head
+
+        while (current && i <= index) {
+            if (i === index) {
+                return current
+            }
+
+            current = current.next
+            i++
+        }
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    addAtHead(val) {
+        const newItem = {
+            value: val
+        }
+
+        if (this.length) {
+            newItem.next = this.head
+            this.head.prev = newItem
+        } else {
+            this.tail = newItem
+        }
+
+        this.head = newItem
+        this.length++
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    addAtTail(val) {
+        const newItem = {
+            value: val
+        }
+
+        if (this.length) {
+            newItem.prev = this.tail
+            this.tail.next = newItem
+        } else {
+            this.head = newItem
+        }
+
+        this.tail = newItem
+        this.length++
+    }
+
+    /**
+     * @param {number} index
+     * @param {number} val
+     * @return {void}
+     */
+    addAtIndex(index, val) {
+        if (index === 0) {
+            this.addAtHead(val)
+        } else if (index === this.length) {
+            this.addAtTail(val)
+        } else if (index < this.length) {
+            const item = this.getNode(index)
+            const newItem = {
+                value: val,
+                next: item,
+                prev: item.prev
+            }
+
+            item.prev.next = newItem
+            item.prev = newItem
+
+            this.length++
+        }
+    }
+
+    /**
+     * @param {number} index
+     * @return {void}
+     */
+    deleteAtIndex(index) {
+        if (this.length) {
+            if (index === 0) {
+                const head = this.head
+
+                if(head.next) {
+                    head.next.prev = undefined
+                    this.head = head.next
+                } else {
+                    this.head = this.tail = undefined
+                }
+            } else if (index === this.length - 1) {
+                const tail = this.tail
+                tail.prev.next = undefined
+                this.tail = tail.prev
+            } else if (index < this.length) {
+                const item = this.getNode(index)
+
+                if (item.prev) {
+                    item.prev.next = item.next
+                }
+
+                if (item.next) {
+                    item.next.prev = item.prev
+                }
+            }
+
+            this.length--
+        }
+    }
+}
